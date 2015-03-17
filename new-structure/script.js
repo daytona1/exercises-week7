f2f3f5ce4f01f9f053209a8ab9b381bd528de630
Andy Dayton <amdayton@gmail.com>
2015-03-17 13:38:04 -0400

    First commit

---
+++ new-structure/script.js
@@ -0,0 +1,16 @@
+/**
+ * Here's where our JavaScript goes
+ */
+
+// define variables here
+// var varName = "";
+
+
+// set up your script here
+function setup() {
+}
+
+
+// this is where the action is
+function draw() {
+}
